4f0930915f9ae4c8b9a5b487becc2e6bfc4aa0a7
Kevin1718 <1718110380@utectulancingo.edu.mx>
2022-06-27 14:29:25 -0500

    /doble

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,11 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 
 });
+
+app.get('/', function (req, res) {
+  res.send('Hello World!\n');
+
+});
 //Este es un comentario 
 
 app.listen(8080, function () {
