ff7cd5faae8addfc5fde3bee9e839fd387e4cd47
Kevin1718 <51490042+Kevin1718@users.noreply.github.com>
2022-06-28 15:03:22 -0500

    Update app.js

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Mi primer hola mundo en node con red hat por Kevin!\n');
 });
 
 app.listen(8080, function () {
